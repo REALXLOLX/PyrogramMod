6b90cffc361b3c08c75cdd2eeab8f766ab45abbf
REALXLOLX <nirmalsinjapati420@gmail.com>
2024-12-21 01:39:33 +0530

    Update send_message.py

--- pyrogram/methods/messages/send_message.py
+++ pyrogram/methods/messages/send_message.py
@@ -38,6 +38,7 @@ class SendMessage:
         partial_reply: str = None,
         schedule_date: datetime = None,
         protect_content: bool = None,
+        message_effect_id: str = None,  # Added message_effect_id parameter
         reply_markup: Union[
             "types.InlineKeyboardMarkup",
             "types.ReplyKeyboardMarkup",
@@ -52,82 +53,15 @@ class SendMessage:
         Parameters:
             chat_id (``int`` | ``str``):
                 Unique identifier (int) or username (str) of the target chat.
-                For your personal cloud (Saved Messages) you can simply use "me" or "self".
-                For a contact that exists in your Telegram address book you can use his phone number (str).
 
             text (``str``):
                 Text of the message to be sent.
 
-            parse_mode (:obj:`~pyrogram.enums.ParseMode`, *optional*):
-                By default, texts are parsed using both Markdown and HTML styles.
-                You can combine both syntaxes together.
-
-            entities (List of :obj:`~pyrogram.types.MessageEntity`):
-                List of special entities that appear in message text, which can be specified instead of *parse_mode*.
-
-            disable_web_page_preview (``bool``, *optional*):
-                Disables link previews for links in this message.
-
-            disable_notification (``bool``, *optional*):
-                Sends the message silently.
-                Users will receive a notification with no sound.
-
-            reply_to_message_id (``int``, *optional*):
-                If the message is a reply, ID of the original message.
-
-            message_thread_id (``int``, *optional*):
-                If the message is in a thread, ID of the original message.
-
-            partial_reply (``str``, *optional*):
-                Text to quote.
-                for reply_to_message only.
-
-            schedule_date (:py:obj:`~datetime.datetime`, *optional*):
-                Date when the message will be automatically sent.
-
-            protect_content (``bool``, *optional*):
-                Protects the contents of the sent message from forwarding and saving.
-
-            reply_markup (:obj:`~pyrogram.types.InlineKeyboardMarkup` | :obj:`~pyrogram.types.ReplyKeyboardMarkup` | :obj:`~pyrogram.types.ReplyKeyboardRemove` | :obj:`~pyrogram.types.ForceReply`, *optional*):
-                Additional interface options. An object for an inline keyboard, custom reply keyboard,
-                instructions to remove reply keyboard or to force a reply from the user.
+            message_effect_id (``str``, *optional*):
+                Unique identifier of the message effect to be added to the message.
 
         Returns:
             :obj:`~pyrogram.types.Message`: On success, the sent text message is returned.
-
-        Example:
-            .. code-block:: python
-
-                # Simple example
-                await app.send_message("me", "Message sent with **Pyrogram**!")
-
-                # Disable web page previews
-                await app.send_message("me", "https://docs.pyrogram.org",
-                    disable_web_page_preview=True)
-
-                # Reply to a message using its id
-                await app.send_message("me", "this is a reply", reply_to_message_id=123)
-
-            .. code-block:: python
-
-                # For bots only, send messages with keyboards attached
-
-                from pyrogram.types import (
-                    ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton)
-
-                # Send a normal keyboard
-                await app.send_message(
-                    chat_id, "Look at that button!",
-                    reply_markup=ReplyKeyboardMarkup([["Nice!"]]))
-
-                # Send an inline keyboard
-                await app.send_message(
-                    chat_id, "These are inline buttons",
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [InlineKeyboardButton("Data", callback_data="callback_data")],
-                            [InlineKeyboardButton("Docs", url="https://docs.pyrogram.org")]
-                        ]))
         """
 
         message, entities = (await utils.parse_text_entities(self, text, parse_mode, entities)).values()
@@ -145,7 +79,8 @@ class SendMessage:
                 reply_markup=await reply_markup.write(self) if reply_markup else None,
                 message=message,
                 entities=entities,
-                noforwards=protect_content
+                noforwards=protect_content,
+                effect_id=message_effect_id  # Added message_effect_id to the payload
             )
         )
 
@@ -185,4 +120,4 @@ class SendMessage:
                     {i.id: i for i in r.users},
                     {i.id: i for i in r.chats},
                     is_scheduled=isinstance(i, raw.types.UpdateNewScheduledMessage)
-                )
+                                  )
